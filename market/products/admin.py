43b9e1ffe0d6493951680ae1aaf9927063054fd4
SpacePeacemaker <dimbo2007@yandex.ru>
2023-11-12 22:37:21 +0500

    12.11.2023 - product model extension, add image model

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin  # noqa F401
 
-from .models import Category
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description_short", "is_active"
+    list_display_links = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "description_short", "date_of_publication"
     list_display_links = ("name",)
--- market/products/models.py
+++ market/products/models.py
@@ -2,30 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Product(models.Model):
-    """Продукт"""
-
-    name = models.CharField(max_length=512, verbose_name=_("наименование"))
-    details = models.ManyToManyField("Detail", through="ProductDetail", verbose_name=_("характеристики"))
-
-
-class Detail(models.Model):
-    """Свойство продукта"""
-
-    name = models.CharField(max_length=512, verbose_name=_("наименование"))
-
-
-class ProductDetail(models.Model):
-    """Значение свойства продукта"""
-
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    detail = models.ForeignKey(Detail, on_delete=models.CASCADE)
-    value = models.CharField(max_length=128, verbose_name=_("значение"))
-
-    class Meta:
-        constraints = [models.UniqueConstraint("product", "detail", name="unique_detail_for_product")]
-
-
 class Category(models.Model):
     """Категория"""
 
@@ -43,3 +19,48 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+
+class Product(models.Model):
+    """Продукт"""
+
+    name = models.CharField(max_length=512, verbose_name=_("наименование"))
+    description = models.TextField(verbose_name=_("описание"), blank=True)
+    number_of_purchases = models.IntegerField(default=0)
+    date_of_publication = models.DateField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    details = models.ManyToManyField("Detail", through="ProductDetail", verbose_name=_("характеристики"))
+
+    @property
+    def description_short(self) -> str:
+        if len(self.description) < 50:
+            return self.description
+        return self.description[:48] + "..."
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Detail(models.Model):
+    """Свойство продукта"""
+
+    name = models.CharField(max_length=512, verbose_name=_("наименование"))
+
+
+class ProductDetail(models.Model):
+    """Значение свойства продукта"""
+
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    detail = models.ForeignKey(Detail, on_delete=models.CASCADE, verbose_name=_("характеристика"))
+    value = models.CharField(max_length=128, verbose_name=_("значение"))
+
+    class Meta:
+        constraints = [models.UniqueConstraint("product", "detail", name="unique_detail_for_product")]
+
+
+class ProductImage(models.Model):
+    """Фотографии продукта"""
+
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name=_("изображение"), blank=True)
+    parent_id = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
