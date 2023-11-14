2fc6416d50455f1f33a3cc45ca87f52be5b5b3da
dmotryshalaiko <demon_at@mail.ru>
2023-11-14 23:16:31 +0300

    Добавил тестирование

--- market/config/settings.py
+++ market/config/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "uploads"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
