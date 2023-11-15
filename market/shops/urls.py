9e1c30847e80413fc106ede5145aa44054d078e0
dmotryshalaiko <demon_at@mail.ru>
2023-11-15 23:34:12 +0300

    Разработка делтальной страницы магазинов

--- market/shops/urls.py
+++ market/shops/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ShopPageView
-
+from .views import (
+    ShopPageView,
+    ShopDetailView,
+)
 
 app_name = "shops"
 
 
 urlpatterns = [
     path("", ShopPageView.as_view(), name="shops"),
+    path("shops/<int:pk>/", ShopDetailView.as_view(), name="shops_detail"),
 ]
--- market/shops/views.py
+++ market/shops/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render  # noqa F401
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from .models import Shop
 
 
 class ShopPageView(TemplateView):
     template_name = "shops/index.jinja2"
+
+
+class ShopDetailView(DetailView):
+    template_name = "shops/shops_detail.jinja2"
+    queryset = Shop.objects.prefetch_related("products")
