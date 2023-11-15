74b19812bf5bff822b0cc95b314e01c75e5485a9
ivanolshnitsky <ivan.olshnitsky@icloud.com>
2023-11-15 19:22:23 +0300

    const pagination

--- market/config/settings.py
+++ market/config/settings.py
@@ -154,3 +154,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 FIXTURE_DIRS = ["fixtures"]
+
+Paginate = 6
--- market/shops/views.py
+++ market/shops/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render  # noqa F401
 from django.views.generic import TemplateView, ListView
 
+from config.settings import Paginate
 from products.models import Product
 
 
@@ -12,4 +13,4 @@ class ProductListView(ListView):
     template_name = "shops/catalog.jinja2"
     model = Product
     context_object_name = "products"
-    paginate_by = 6
+    paginate_by = Paginate
