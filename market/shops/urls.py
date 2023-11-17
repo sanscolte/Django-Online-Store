313c3eb624d1a954914c1aa0b4c43ffca5312ce3
ivanolshnitsky <ivan.olshnitsky@icloud.com>
2023-11-17 22:09:45 +0300

    refactoring from shops in products

--- market/shops/urls.py
+++ market/shops/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import ProductListView, ShopPageView, ShopDetailView
+from .views import ShopPageView, ShopDetailView
+
 
 app_name = "shops"
 
 
 urlpatterns = [
     path("", ShopPageView.as_view(), name="home"),
-    path("products/", ProductListView.as_view(), name="products"),
     path("shops/<int:pk>/", ShopDetailView.as_view(), name="shops_detail"),
 ]
--- market/shops/views.py
+++ market/shops/views.py
@@ -1,23 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render  # noqa F401
 
-from django.views.generic import TemplateView, ListView, View
-
-from django.conf import settings
-from products.models import Product
+from django.views.generic import TemplateView, View
 
 from .models import Shop
 
 
 class ShopPageView(TemplateView):
     template_name = "shops/index.jinja2"
-
-
-class ProductListView(ListView):
-    template_name = "shops/catalog.jinja2"
-    model = Product
-    context_object_name = "products"
-    paginate_by = settings.PAGINATE_PRODUCTS_BY
 
 
 class ShopDetailView(View):
