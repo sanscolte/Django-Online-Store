32fc3b482217dbe41cba0b2cc6f8f99da37314ca
Timofey Kropotov <timofeykropotov@gmail.com>
2023-11-21 12:08:29 +0300

    media url like uploads, rename fixtures

--- market/config/settings.py
+++ market/config/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 
 STATIC_URL = "/static/"
-MEDIA_URL = "/uploads/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = "uploads/"
+MEDIA_ROOT = BASE_DIR / "uploads"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
