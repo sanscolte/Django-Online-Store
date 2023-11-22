f07bcf750df7d6ab4d3091e9d7063554102e53a2
dmotryshalaiko <demon_at@mail.ru>
2023-11-23 00:16:46 +0300

    Создал новое приложение shops и добавил его в settings.py

--- market/config/settings.py
+++ market/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django_jinja",
     "products",
     "shops",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -152,6 +153,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
 
 FIXTURE_DIRS = ["fixtures"]
 
