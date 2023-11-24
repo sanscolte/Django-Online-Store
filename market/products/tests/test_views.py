e24f1f971e5584e3e381fbda0254e2a8bb406986
Sergey Klimov <ksiu@pkzdrav.ru>
2023-11-24 12:25:17 +0700

    Оптимизировал часть кода

--- market/products/tests/test_views.py
+++ market/products/tests/test_views.py
@@ -1,30 +1,25 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from products.models import Review, Product
+
+User = get_user_model()
 
 
 class ProductDetailReviewTest(TestCase):
     """Класс тестов представлений отзывов детальной страницы продукта"""
 
     fixtures = [
+        "fixtures/01-users.json",
         "fixtures/04-shops.json",
         "fixtures/05-categories.json",
         "fixtures/06-products.json",
         "fixtures/16-reviews.json",
     ]
 
-    @classmethod
-    def setUpClass(cls):
-        cls.user = User.objects.create_user(username="user", password="qwerty")
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.user.delete()
-
     def setUp(self):
-        self.client.force_login(self.user)
+        self.client.force_login(User.objects.get(pk=1))
 
     def test_view_context(self):
         """Тестирование передачи данных в контексте"""
@@ -35,8 +30,11 @@ class ProductDetailReviewTest(TestCase):
 
         print(reviews_context, product_context)
 
-        response = self.client.get(reverse("products:product", args=(pk,)))
+        response = self.client.get(reverse("products:product-detail", args=(pk,)))
 
+        self.assertEqual(response.status_code, 200)
+
+        # FIXME перед ассертами отпринтуй response.context и response.json(), чтобы понимать что с чем сравнивать
         self.assertEqual(response.context["reviews"], reviews_context)
         self.assertEqual(response.context["product"], product_context)
 
@@ -49,5 +47,9 @@ class ProductDetailReviewTest(TestCase):
             "rating": 5,
         }
 
-        response = self.client.post(reverse("products:product", args=(pk,)), data=form)
+        response = self.client.post(reverse("products:product-detail", args=(pk,)), data=form)
+
+        self.assertRedirects(response, reverse("products:product-detail", args=(pk,)))
+
+        # FIXME перед ассертом отпринтуй response.context и response.json(), чтобы понимать что с чем сравнивать
         self.assertEqual(response.context["success"], True)
