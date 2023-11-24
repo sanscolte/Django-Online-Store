504be137990bae146db0268d4cd793435f86f59f
Timofey Kropotov <timofeykropotov@gmail.com>
2023-11-24 06:05:56 +0300

    FormView changed to DetailView

--- market/products/views.py
+++ market/products/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpRequest
 from django.shortcuts import render, redirect  # noqa F401
 
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, DetailView
 from django.conf import settings
 
 from products.models import Product, Review
@@ -14,19 +15,30 @@ class ProductListView(ListView):
     paginate_by = settings.PAGINATE_PRODUCTS_BY
 
 
-class ProductDetailView(FormView):
+class ProductDetailView(DetailView):
     template_name = "products/product_detail.jinja2"
-    form_class = ReviewForm
-    success_url = "#"
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        form.instance.product = Product.objects.get(pk=self.kwargs.get("pk"))
-        form.save()
-        return super().form_valid(form)
+    model = Product
+    context_object_name = "product"
 
     def get_context_data(self, **kwargs):
         context = super(ProductDetailView, self).get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(product__pk=self.kwargs.get("pk"))
-        context["product"] = Product.objects.get(pk=self.kwargs.get("pk"))
+        context["form"] = ReviewForm()
         return context
+
+    def post(self, request: HttpRequest, **kwargs):
+        form = ReviewForm(request.POST)
+        form.instance.user = self.request.user
+        form.instance.product = Product.objects.get(pk=self.kwargs.get("pk"))
+
+        if form.is_valid():
+            form.save()
+        else:
+            form = ReviewForm()
+
+        context = {
+            "form": form,
+            "reviews": Review.objects.filter(product__pk=self.kwargs.get("pk")),
+            "product": Product.objects.get(pk=self.kwargs.get("pk")),
+        }
+        return render(request, "products/product_detail.jinja2", context)
