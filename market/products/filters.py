05e6defbb55a308c8a5614c923aac885635aba8f
Sergey Klimov <ksiu@pkzdrav.ru>
2023-12-01 18:45:35 +0700

    Разработан класс фильтра продуктов по наименованию и средней цене

--- market/products/filters.py
+++ market/products/filters.py
@@ -1,8 +1,13 @@
+from django.db.models import Avg
+from django.db.models.functions import Round
 from django_filters import FilterSet
 from .models import Product
 
 
 class ProductFilter(FilterSet):
     class Meta:
-        model = Product
-        fields = ["name", "offers__price"]
+        queryset = Product.objects.annotate(avg_price=Round(Avg("offers__price"), 2))
+        fields = {
+            "avg_price": ["lte", "gte"],
+            "name": ["icontains"],
+        }
