2cc23fd91dc25e39d807d8c8f755e24cb1fe5e86
timmy <timofeykropotov@gmail.com>
2023-12-14 22:23:27 +0300

    form.is_valid() not working

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,6 +1,25 @@
-from django.contrib import admin  # noqa F401
+import json
+import logging
+import os.path
 
+from django.contrib import admin  # noqa F401
+from django.core.files.storage import FileSystemStorage
+from django.core.management import call_command
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
+from django.urls import path
+
+from .forms import ProductImportForm
 from .models import Category, Product, Banner, Review, ProductImage, ProductDetail, Detail, ProductsViews
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(os.path.join("logs/import/products-import.log"))
+formatter = logging.Formatter("%(asctime)s - %(product)s - %(category)d - %(is_success)s", datefmt="%d-%b-%y %H:%M:%S")
+
+logger.addHandler(file_handler)
+file_handler.setFormatter(formatter)
 
 
 @admin.register(Category)
@@ -13,6 +32,48 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description_short", "date_of_publication", "category_id"
     list_display_links = ("name",)
+    change_list_template = "products/products_changelist.html"
+
+    def import_json(self, request: HttpRequest) -> HttpResponse:
+        if request.method == "GET":
+            form = ProductImportForm()
+            context = {
+                "form": form,
+            }
+            return render(request, "admin/product-import-form.html", context)
+
+        form = ProductImportForm(request.POST, request.FILES)
+
+        if not form.is_valid():
+            context = {
+                "form": form,
+            }
+            return render(request, "admin/product-import-form.html", context, status=400)
+
+        file = form.cleaned_data["json_file"]
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        call_command("loaddata", file.name)
+
+        print("saved file", filename)
+        self.message_user(request, "Data from file was imported")
+
+        with open(os.path.join(f"uploads/{filename}"), "r") as json_file:
+            json_file = json_file.read()
+            data = json.loads(json_file)
+
+        for product in data:
+            product_name = product["fields"]["name"]
+            category = product["fields"]["category"]
+            is_success = "SUCCESS"
+            logger.debug("", extra={"product": product_name, "category": category, "is_success": is_success})
+
+        return redirect("..")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path("import-products/", self.import_json, name="import_products")]
+        return new_urls + urls
 
 
 @admin.register(Banner)
--- market/products/forms.py
+++ market/products/forms.py
@@ -30,3 +30,27 @@ class ProductImageForm(forms.ModelForm):
     class Meta:
         model = ProductImage
         fields = "image", "sort_image"
+
+
+class ProductImportForm(forms.Form):
+    json_file = forms.FileField()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        json_file = cleaned_data.get("json_file")
+
+        if not json_file.name.endswith(".json"):
+            raise forms.ValidationError("Файл должен быть в формате JSON")
+        return cleaned_data
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if not name:
+            raise forms.ValidationError("Наименование обязательно для заполнения")
+        return name
+
+    def clean_category(self):
+        category = self.cleaned_data.get("category")
+        if not category:
+            raise forms.ValidationError("Категория обязательна для заполнения")
+        return category
