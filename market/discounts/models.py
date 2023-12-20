cd195eb53a4ee65511b166a83c326e1f01a54245
ivanolshnitsky <ivan.olshnitsky@icloud.com>
2023-12-20 16:51:47 +0300

    add def str and verbose and plural name

--- market/discounts/models.py
+++ market/discounts/models.py
@@ -21,6 +21,20 @@ class DiscountBase(models.Model):
 class DiscountProduct(DiscountBase):
     products = models.ManyToManyField(Product, related_name="discount_products")
 
+    class Meta:
+        verbose_name = "Скидка для продукта"
+        verbose_name_plural = "Скидки для продуктов"
+
+    def __str__(self):
+        return f"DiscountProduct(pk={self.pk}, product={self.products.values_list('name')[0][0]})"
+
 
 class DiscountSet(DiscountBase):
     categories = models.ManyToManyField(Category, related_name="discount_sets")
+
+    class Meta:
+        verbose_name = "Скидка для набора"
+        verbose_name_plural = "Скидки на наборов"
+
+    def __str__(self):
+        return f"DiscountSet(pk={self.pk}, categories={self.categories.values_list('name')[::1]})"
