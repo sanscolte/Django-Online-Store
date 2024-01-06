bfdb123f3d29fafb59618130e0ca850e3c5963d2
dmotryshalaiko <demon_at@mail.ru>
2024-01-07 00:01:48 +0300

    Поменял названия в verbose name на русские

--- market/orders/models.py
+++ market/orders/models.py
@@ -29,35 +29,35 @@ class Status(models.TextChoices):
 class Order(models.Model):
     """Модель заказа с информацией о пользователе"""
 
-    full_name = models.CharField(max_length=50, verbose_name="full name")
+    full_name = models.CharField(max_length=50, verbose_name="полное имя")
     email = models.EmailField(verbose_name="email")
-    phone_number = models.CharField(max_length=12, unique=True, verbose_name="phone")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="created at")
+    phone_number = models.CharField(max_length=12, unique=True, verbose_name="телефон")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="создан")
     delivery_type = models.CharField(
         max_length=50,
         choices=DeliveryTypes.choices,
-        verbose_name="delivery type",
+        verbose_name="доставка",
         blank=False,
         default=DeliveryTypes.REGULAR,
     )
-    address = models.CharField(max_length=250, verbose_name="address")
-    city = models.CharField(max_length=50, verbose_name="city")
+    address = models.CharField(max_length=250, verbose_name="адрес")
+    city = models.CharField(max_length=50, verbose_name="город")
     payment_type = models.CharField(
         max_length=50,
         choices=PaymentTypes.choices,
         blank=False,
         default=PaymentTypes.CARD,
-        verbose_name="payment type",
+        verbose_name="оплата",
     )
     status = models.CharField(
-        max_length=15, choices=Status.choices, default=Status.STATUS_CREATED, verbose_name="status"
+        max_length=15, choices=Status.choices, default=Status.STATUS_CREATED, verbose_name="статус"
     )
 
 
 class OrderItem(models.Model):
     """Модель заказов с продуктами"""
 
-    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name="order_items", on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE, verbose_name="заказ")
+    product = models.ForeignKey(Product, related_name="order_items", on_delete=models.CASCADE, verbose_name="продукт")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="цена")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="количество")
