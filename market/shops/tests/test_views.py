5b1f1830c70f356c7aa470f0d997c210aa42104c
ivanolshnitsky <ivan.olshnitsky@icloud.com>
2024-01-25 10:50:31 +0300

    add fixtures

--- market/shops/tests/test_views.py
+++ market/shops/tests/test_views.py
@@ -26,9 +26,13 @@ class BannerInIndexPageViewTest(TestCase):
     """Класс тестов представления домашней страницы"""
 
     fixtures = [
+        "fixtures/04-shops.json",
         "fixtures/05-categories.json",
         "fixtures/06-products.json",
+        "fixtures/08-offers.json",
+        "fixtures/11-product-images.json",
         "fixtures/15-banners.json",
+        "fixtures/21-discount-products.json",
     ]
 
     @classmethod
@@ -38,15 +42,15 @@ class BannerInIndexPageViewTest(TestCase):
         super(BannerInIndexPageViewTest, cls).setUpClass()
         cls.images: list[str] = list(banner.image.url for banner in Banner.objects.all())
 
-    # def test_banners_in_page(self):
-    #     """Проверка на наличие трех шаблонов в представлении, проверка статус-кода ответа"""
-    #
-    #     response = self.client.get(reverse("shops:home"))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     for image in self.images:
-    #         if image in str(response.content):
-    #             return True
-    #
-    #     for _ in range(3):
-    #         self.assertContains(response, "Slider-item")
+    def test_banners_in_page(self):
+        """Проверка на наличие трех шаблонов в представлении, проверка статус-кода ответа"""
+
+        response = self.client.get(reverse("shops:home"))
+        self.assertEqual(response.status_code, 200)
+
+        for image in self.images:
+            if image in str(response.content):
+                return True
+
+        for _ in range(3):
+            self.assertContains(response, "Slider-item")
