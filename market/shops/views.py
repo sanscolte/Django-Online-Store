ed9594d6e240059b4f3e679e90dddff5531bc590
ivanolshnitsky <ivan.olshnitsky@icloud.com>
2024-01-25 12:27:03 +0300

    add cart to home page

--- market/shops/views.py
+++ market/shops/views.py
@@ -5,13 +5,15 @@ from django.views.decorators.cache import cache_page
 
 from django.views.generic import TemplateView, View
 
+from cart.forms import CartAddProductForm
+from cart.services import CartServices
 from discounts.models import DiscountProduct
 from products.constants import KEY_FOR_CACHE_PRODUCTS
 from .models import Shop, Offer
 
 import random
 from config.settings import CACHE_TIME
-from products.models import Banner
+from products.models import Banner, Product
 
 
 @method_decorator(cache_page(60 * 5, key_prefix=KEY_FOR_CACHE_PRODUCTS), name="dispatch")
@@ -28,7 +30,23 @@ class IndexPageView(TemplateView):
         context["cache_time"] = CACHE_TIME
         context["discount_product"] = random.choice(DiscountProduct.objects.all())
         context["filtered_offers"] = Offer.objects.filter(remains__lte=50)
+        context["cart_form"] = CartAddProductForm(initial={"quantity": 1, "update": False})
         return context
+
+    def post(self, request: HttpRequest, **kwargs):
+        cart_form = CartAddProductForm(request.POST)
+        if cart_form.is_valid():
+            product_name = request.POST["product_name"]
+            product = Product.objects.get(name=product_name)
+            quantity = cart_form.cleaned_data["quantity"]
+            cart_services = CartServices(request)
+            cart_services.add(
+                product=product,
+                shop=None,
+                quantity=quantity,
+                update_quantity=True,
+            )
+        return redirect("shops:home")
 
 
 class ShopDetailView(View):
