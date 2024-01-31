6a47898edd9f0c43fe990fcba74a2e96e5907e51
timmy <timofeykropotov@gmail.com>
2024-01-31 13:42:51 +0300

    migrations are reduced to a single file

--- market/payment/migrations/0001_initial.py
+++ market/payment/migrations/0001_initial.py
@@ -7,10 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = [
-        ("orders", "0002_order_total_price"),
-    ]
-
     operations = [
         migrations.CreateModel(
             name="BankTransaction",
